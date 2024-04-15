9252399586e2b5eb24aecfa49ba70f3b21dba828
kdgyun <kdonggyun97@gmail.com>
2024-04-15 15:18:20 +0900

    init

---
+++ frontend/src/api.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+// Axios 인스턴스 생성
+const api = axios.create({
+  baseURL: 'http://localhost:8000/api',
+  xsrfHeaderName: 'X-CSRFToken',
+  xsrfCookieName: 'csrftoken'
+});
+
+// 전역 에러 핸들링 함수
+function handleError(error) {
+  console.error("API Error:", error.response.data);
+  // 오류에 따른 추가적인 액션을 취할 수 있습니다.
+}
+
+export function getHealthCheck() {
+    return ajax('/account/healthcheck?format=json', 'get');
+}
+  
+// ajax 함수
+function ajax(url, method, { params = {}, data = {} } = {}) {
+  return new Promise((resolve, reject) => {
+    api({ url, method, params, data }).then(response => {
+      if (response.data.error) {
+        reject(new Error(response.data.data));
+      } else {
+        resolve(response);
+      }
+    }).catch(error => {
+      reject(error);
+    });
+  });
+}
