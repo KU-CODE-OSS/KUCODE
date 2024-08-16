896d8811c349748fd777883f58b260f39558d580
okhs0712 <okhs0712@naver.com>
2024-08-16 19:52:02 +0900

    repo 수정2

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
+const ip_for_develop = 'http://61.72.142.8:10004/api'
 // Axios 인스턴스 생성
 const api = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
   xsrfHeaderName: 'X-CSRFToken',
   xsrfCookieName: 'csrftoken'
 });
+
+const headers = {
+  'Content-Type': 'multipart/form-data'
+};
 
 // 전역 에러 핸들링 함수
 function handleError(error) {
@@ -16,11 +21,27 @@ function handleError(error) {
 export function getHealthCheck() {
     return ajax('/account/healthcheck', 'get');
 }
-  
+
+export function getCourseInfo() {
+  return ajax(ip_for_develop + '/account/student_read_course_info', 'get')
+}
+
+export function getCourseTotalInfo() {
+  return ajax(ip_for_develop + '/account/student_read_total', 'get')
+}
+
+export function postCourseUpload(formData) {
+  return ajax(ip_for_develop + '/account/student_excel_import', 'post', { data: formData, headers })
+}
+
+export function getRepoInfo() {
+  return ajax(ip_for_develop + '/repo/repo_read_db', 'get')
+}
+
 // ajax 함수
-function ajax(url, method, { params = {}, data = {} } = {}) {
+function ajax(url, method, { params = {}, data = {}, headers = {} } = {}) {
   return new Promise((resolve, reject) => {
-    api({ url, method, params, data }).then(response => {
+    api({ url, method, params, data, headers }).then(response => {
       if (response.data.error) {
         reject(new Error(response.data.data));
       } else {
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,110 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router';
+import Dashboard from '@/views/Dashboard.vue';
+import Information from '@/views/Information.vue';
+import Statistics from '@/views/Statistics.vue';
+import StatisticsTotal from '@/views/StatisticsComponents/StatisticsTotal.vue';
+import StatisticsCourse from '@/views/StatisticsComponents/StatisticsCourse.vue';
+import StatisticsDepartment from '@/views/StatisticsComponents/StatisticsDepartment.vue';
+import InformationCourse from '@/views/InformationComponents/InformationCourse.vue'
+import InformationRepos from '@/views/InformationComponents/InformationRepos.vue'
+import InformationStudent from '@/views/InformationComponents/InformationStudent.vue'
+import QnA from '../views/QnA.vue'
+
+const routes = [
+  {
+    path: '/board',
+    name: 'board',
+    component: Dashboard
+  },
+  {
+    path: '/info',
+    name: 'Information',
+    component: Information,
+    children: [
+      { // default path
+        path: '',
+        redirect: '/info/course',
+      },
+      {
+        path: 'course',
+        name: 'InformationCourse',
+        component: InformationCourse,
+        props: true,
+      },
+      {
+        path: 'students',
+        name: 'InformationStudent',
+        component: InformationStudent,
+        props: true,
+      },
+      {
+        path: 'repos',
+        name: 'InformationRepos',
+        component: InformationRepos,
+      },
+    ]
+  },
+  {
+    path: '/statistics',
+    name: 'Statistics',
+    component: Statistics,
+    children: [
+      { // default path
+        path: '',
+        redirect: '/statistics/total',
+      },
+      {
+        path: 'total',
+        name: 'StatisticsTotal',
+        component: StatisticsTotal,
+        props: true,
+      },
+      {
+        path: 'course',
+        name: 'StatisticsCourse',
+        component: StatisticsCourse,
+      },
+      {
+        path: 'department',
+        name: 'StatisticsDepartment',
+        component: StatisticsDepartment,
+      },
+    ]
+  },
+  {
+    path: '/qna',
+    name: 'QnA',
+    component: QnA
+  }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+
+  const defaultQuery = { page: '1', type: 'summary' };
+
+  if (to.name === "InformationStudent") {
+    const query = { ...to.query };
+    if (!query.page) {
+      query.page = defaultQuery.page;
+    }
+    if (!query.type) {
+      query.type = defaultQuery.type;
+    }
+    if (query.page !== to.query.page || query.type !== to.query.type) {
+      next({ name: to.name, params: to.params, query: query });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+
+export default router;
