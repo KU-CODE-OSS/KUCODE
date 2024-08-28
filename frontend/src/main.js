ba4e5ac2ab7c419485e53a2f390e12e98d3fbebc
kdgyun <kdonggyun97@gmail.com>
2024-08-28 13:57:16 +0900

    update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,31 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-// import ECharts from 'vue-echarts'
-// import 'echarts-gl'
-import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
-// import echarts from 'echarts'
-// import { LineChart, BarChart, ScatterChart, EffectScatterChart } from 'echarts/charts'
-// import { GridComponent, TitleComponent, TooltipComponent, LegendComponent, DataZoomComponent } from 'echarts/components'
-// import { UniversalTransition } from 'echarts/features'
-// import { CanvasRenderer } from 'echarts/renderers'
-// echarts.use([
-//     CanvasRenderer,
-//     TitleComponent,
-//     LineChart,
-//     BarChart,
-//     ScatterChart,
-//     GridComponent,
-//     TooltipComponent,
-//     EffectScatterChart,
-//     UniversalTransition,
-//     LegendComponent,
-//     DataZoomComponent
-//   ])
+
+// import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+
 
 const app = createApp(App)
-// app.component('v-chart', VueECharts)
-app.component('pulse-loader', PulseLoader)
+// app.component('pulse-loader', PulseLoader)
 app.use(vuetify)
 app.use(createPinia())
 app.use(router)
